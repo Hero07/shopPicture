584fa17a5176f15638bd184714bfb0318d142d82
Hero07 <jessemannu@gmail.com>
2019-11-20 12:50:49 -0600

    Updates

---
+++ precache-manifest.329e8aeafc5aefc79534ae6f3f7024a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1230582793cea56d0e8337f5a6323f9",
+    "url": "/Hero07/shopPicture/index.html"
+  },
+  {
+    "revision": "dc04c1bd75f304d53923",
+    "url": "/Hero07/shopPicture/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "cc803fba2e111c879500",
+    "url": "/Hero07/shopPicture/static/js/2.a5bd7084.chunk.js"
+  },
+  {
+    "revision": "dc04c1bd75f304d53923",
+    "url": "/Hero07/shopPicture/static/js/main.dbd5205d.chunk.js"
+  },
+  {
+    "revision": "3998ed1a332b596258f1",
+    "url": "/Hero07/shopPicture/static/js/runtime-main.4e23a157.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Hero07/shopPicture/static/media/logo.25bf045c.svg"
+  }
+]);
